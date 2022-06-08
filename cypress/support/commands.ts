4f08f7f9919245bdeb718028f68d77e62add726f
Taylor Bantle <taylor@dolthub.com>
2022-06-08 10:24:53 -0700

    Set up typescript, cypress, test runs

---
+++ cypress/support/commands.ts
@@ -0,0 +1,105 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+export const defaultTimeout = 10000;
+
+// Ensures page has loaded before running tests
+// Reference: https://www.cypress.io/blog/2018/02/05/when-can-the-test-start/
+Cypress.Commands.add("visitAndWait", (path: string) => {
+  let appHasStarted = false;
+
+  function spyOnAddEventListener(win: any) {
+    // win = window object in our application
+    const addListener = win.EventTarget.prototype.addEventListener;
+    // eslint-disable-next-line no-param-reassign
+    win.EventTarget.prototype.addEventListener = function (name: string) {
+      if (name === "change") {
+        // web app added an event listener to the input box -
+        // that means the web application has started
+        appHasStarted = true;
+        // restore the original event listener
+        // eslint-disable-next-line no-param-reassign
+        win.EventTarget.prototype.addEventListener = addListener;
+      }
+      // eslint-disable-next-line prefer-rest-params
+      return addListener.apply(this, arguments);
+    };
+  }
+
+  function waitForAppStart() {
+    // keeps rechecking "appHasStarted" variable
+    return new Cypress.Promise((resolve, _) => {
+      // eslint-disable-next-line consistent-return
+      const isReady = () => {
+        if (appHasStarted) {
+          return resolve();
+        }
+        setTimeout(isReady, 0);
+      };
+      isReady();
+    });
+  }
+
+  cy.visit(path, {
+    onBeforeLoad: spyOnAddEventListener,
+    failOnStatusCode: false,
+  }).then(waitForAppStart);
+});
+
+Cypress.Commands.add("handleGoogle", () => {
+  // create the stub here
+  const ga = cy.stub().as("ga");
+
+  // prevent google analytics from loading and replace it with a stub before every
+  // single page load including all new page navigations
+  cy.on("window:before:load", (win) => {
+    if (!Object.getOwnPropertyDescriptor(win, "ga")) {
+      Object.defineProperty(win, "ga", {
+        configurable: false,
+        get: () => ga, // always return the stub
+        set: () => {}, // don't allow actual google analytics to overwrite this property
+      });
+    }
+  });
+});
+
+Cypress.Commands.add("visitPage", (currentPage: string) => {
+  cy.handleGoogle();
+
+  // 404 page should be rendered when page not found
+  cy.visitAndWait(currentPage);
+});
+
+Cypress.Commands.add("visitViewport", (device: Cypress.ViewportPreset) => {
+  cy.viewport(device);
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(500);
+});
+
+Cypress.on(
+  "uncaught:exception",
+  (err) => !err.message.includes("ResizeObserver loop limit exceeded")
+);
