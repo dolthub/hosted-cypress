835de1c11985b83debe5059a05cfe7d9a91acdc2
Taylor Bantle <taylor@dolthub.com>
2025-06-26 06:37:29 -0700

    Fix cypress config

--- eslint.config.js
+++ eslint.config.js
@@ -1,112 +1,112 @@
-const js = require('@eslint/js');
-const typescriptEslint = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
-const cypress = require('eslint-plugin-cypress');
-const _import = require('eslint-plugin-import');
+const js = require("@eslint/js");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const cypress = require("eslint-plugin-cypress");
+const _import = require("eslint-plugin-import");
 
 module.exports = [
   js.configs.recommended,
   {
-    ignores: ['dist/**/*', 'node_modules/**/*', '**/*.js'],
+    ignores: ["dist/**/*", "node_modules/**/*", "**/*.js", "cypress.config.ts"],
   },
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     plugins: {
-      '@typescript-eslint': typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
       cypress,
       import: _import,
     },
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: "./tsconfig.json",
         ecmaVersion: 2020,
-        sourceType: 'module',
+        sourceType: "module",
       },
       globals: {
-        describe: 'readonly',
-        it: 'readonly',
-        xit: 'readonly',
-        expect: 'readonly',
-        cy: 'readonly',
-        Cypress: 'readonly',
-        before: 'readonly',
-        beforeEach: 'readonly',
-        after: 'readonly',
-        afterEach: 'readonly',
-        setTimeout: 'readonly',
-        clearTimeout: 'readonly',
-        setInterval: 'readonly',
-        clearInterval: 'readonly',
+        describe: "readonly",
+        it: "readonly",
+        xit: "readonly",
+        expect: "readonly",
+        cy: "readonly",
+        Cypress: "readonly",
+        before: "readonly",
+        beforeEach: "readonly",
+        after: "readonly",
+        afterEach: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
       },
     },
     rules: {
       ...typescriptEslint.configs.recommended.rules,
       ...cypress.configs.recommended.rules,
-      
+
       // TypeScript-specific rules
-      '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
-      '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/naming-convention': [
-        'error',
+      "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/naming-convention": [
+        "error",
         {
-          selector: 'default',
-          format: ['camelCase'],
-          leadingUnderscore: 'allow',
+          selector: "default",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
         },
         {
-          selector: 'variable',
-          format: ['camelCase', 'UPPER_CASE'],
+          selector: "variable",
+          format: ["camelCase", "UPPER_CASE"],
         },
         {
-          selector: ['typeLike', 'enumMember'],
-          format: ['PascalCase'],
+          selector: ["typeLike", "enumMember"],
+          format: ["PascalCase"],
         },
         {
-          selector: ['objectLiteralProperty'],
+          selector: ["objectLiteralProperty"],
           format: null,
         },
       ],
-      '@typescript-eslint/no-base-to-string': 'error',
-      '@typescript-eslint/no-extra-non-null-assertion': 'error',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-namespace': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
+      "@typescript-eslint/no-base-to-string": "error",
+      "@typescript-eslint/no-extra-non-null-assertion": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-namespace": "error",
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
         {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
         },
       ],
-      '@typescript-eslint/no-use-before-define': [
-        'error',
+      "@typescript-eslint/no-use-before-define": [
+        "error",
         {
           classes: false,
           functions: false,
         },
       ],
-      '@typescript-eslint/prefer-nullish-coalescing': ['warn'],
-      '@typescript-eslint/prefer-optional-chain': 'error',
-      '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-      '@typescript-eslint/promise-function-async': 'error',
-      
+      "@typescript-eslint/prefer-nullish-coalescing": ["warn"],
+      "@typescript-eslint/prefer-optional-chain": "error",
+      "@typescript-eslint/prefer-string-starts-ends-with": "error",
+      "@typescript-eslint/promise-function-async": "error",
+
       // General rules
-      'import/prefer-default-export': 'off',
-      'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-      'no-sequences': 'error',
-      'no-underscore-dangle': 'off',
-      'no-use-before-define': ['error', { classes: false, functions: false }],
-      
+      "import/prefer-default-export": "off",
+      "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+      "no-sequences": "error",
+      "no-underscore-dangle": "off",
+      "no-use-before-define": ["error", { classes: false, functions: false }],
+
       // Cypress rules
-      'cypress/no-unnecessary-waiting': 'warn',
+      "cypress/no-unnecessary-waiting": "warn",
     },
     settings: {
-      'import/resolver': {
+      "import/resolver": {
         node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
         },
       },
     },
